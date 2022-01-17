8f129b25b990cc58047022e0bf02cb06e16ffe74
Jennifer Perez <jenniferperez@pursuit.org>
2022-01-17 10:36:26 -0600

    cat app renders three random quotes from api and generates new ones when button clicked using hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+// import React,{useState,useEffect} from 'react';
 
 function App() {
+
+  const [catFacts,setCatFacts]=React.useState([])
+
+  React.useEffect(()=>{
+    getNewCatfact();
+  }, [])
+
+  const getNewCatfact=()=>{
+
+    if(catFacts.length>2){
+    setCatFacts(catFacts.slice(1))
+  }
+
+  fetch("https://catfact.ninja/fact?max_length=140")
+    .then(res => res.json())
+    .then(
+      (result)=>{
+        console.log(result);
+        setCatFacts(catFacts=>[...catFacts,result]);
+        // Note:...catch errors, instead of catch() 
+      },  
+      (error)=>{
+        //setIsLoaded(true);
+        //setError(Error);
+      }
+    
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    {/* Create a new application
+    get a single cat fact from catFacts.ninja
+    render it to the screen
+    Create a button to get a new catfact and add it to the list of catFacts
+    if there are more than 3 facts when user clicks the button, delete the oldest one */}
+    <div className='App'>
+      {catFacts.length > 0 && 
+        catFacts.map((catFact,index)=>{
+            return(
+             
+              
+              <div className="catFact" key={index}>{catFact.fact}</div>
+
+            )
+        })
+      }
+      <div className="button" onClick={()=>getNewCatfact()}>button</div>
     </div>
+   
+       
+    </>
   );
-}
+ }
 
 export default App;
